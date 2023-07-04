4915a99cecbfdedd61a895806546b69b8f1db1b6
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-07-04 13:22:52 -0500

    function to make a  new Dashboard

--- server/src/services/dashboards.service.js
+++ server/src/services/dashboards.service.js
@@ -1,13 +1,54 @@
 const {getClientMongo} = require('./bd')
 const {ObjectId} = require('mongodb')
 
-const getDashboardByIdUser=async(id_user)=>{
+const getDashboardsByIdUser=async(id_user)=>{
     const client = getClientMongo()
     await client.connect()
-    return  client.db('webadtasker').collection('dashboard').findOne({"id_aut":new ObjectId(id_user)})
-
+    return  client.db('webadtasker').collection('dashboards').find({"id_aut":new ObjectId(id_user)}).toArray()
 }
 
+const creatNewDashboard = async(nombre,descripcion,id_user)=>{
+    const client = await getClientMongo().connect()
+    dash = {
+            id_aut:new ObjectId(id_user),
+            nombre,
+            descripcion,
+            sesions:[
+                {
+                    _id:new ObjectId(),
+                    nombre:"Todo",
+                    tasks:[
+                        {
+                            _id:new ObjectId(),
+                            title:"Welcome !",
+                            descripcion:"First task"
+                        }
+                    ]
+                }
+                ,{
+                    _id:new ObjectId(),
+                    nombre:"In Progress",
+                    tasks:[
+                        
+                    ]
+
+                }
+                ,{
+                    _id:new ObjectId(),
+                    nombre:"Terminate",
+                    tasks:[
+                        
+                    ]
+
+                }
+            ]
+
+    }
+    resp =  await  client.db('webadtasker').collection('dashboards').insertOne(dash)
+    dash["_id"]=resp.insertedId
+    return dash
+ 
+}   
 
 
-module.exports = {getDashboardByIdUser}
+module.exports = {getDashboardsByIdUser,creatNewDashboard}
--- server/src/v1/routes/dashBoardRoutes.js
+++ server/src/v1/routes/dashBoardRoutes.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const {getDashboard,newDashboard} = require('../../controllers/dashboardController')
+const {validateToken} = require('../../middlewares/valdiateToken')
 const router = Router()
 
 router.get('/getDashboard/:id_user',getDashboard)
 //router.patch('/updateDashboard/user/:id_user/d/:id_dashboard')
-router.patch('/newDashboard/user/:id_user',newDashboard)
+router.post('/newDashboard/user/:id_user/name/:name_dashboard',validateToken,newDashboard)
 
 module.exports = router;

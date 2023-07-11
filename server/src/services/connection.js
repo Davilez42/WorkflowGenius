dcef823642b5e96f239d8e653460231016d898bc
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-07-11 08:05:42 -0500

    refactor:connection to databse

--- server/src/services/connection.js
+++ server/src/services/connection.js
@@ -1,13 +1,16 @@
-const config = require('../configs/config')
-const mongoose  =  require('mongoose') 
+const config = require("../configs/config");
+const mongoose = require("mongoose");
 
-mongoose.connect(config.URI_DB)
-.then(()=>{
-    mongoose.connection.on("open",_=>console.log("open connection"))
-    mongoose.connection.on("error",err=>console.log(`error connection : ${err}`))
-})
-.catch(err=>{
+mongoose
+  .connect(config.URI_DB)
+  .then(() => {
+    mongoose.connection.on("open", (_) => console.log("open connection"));
+    mongoose.connection.on("error", (err) =>
+      console.log(`error connection : ${err}`)
+    );
+  })
+  .catch((err) => {
     console.log(`Error connect at  database ${config.URI_DB}`);
-})
+  });
 
-module.exports = mongoose
+module.exports = mongoose;

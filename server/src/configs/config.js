d1ba1e1fd5a5983ede990af4a46dd26a2426d2ed
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-07-11 08:09:08 -0500

    ci(config):changed ip

--- server/src/configs/config.js
+++ server/src/configs/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT:5000,
-    HOST:'192.168.1.7',
-    URI_DB:"mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.1",
+    HOST:'localhost',
+    URI_DB:"mongodb://127.0.0.1:27017/webadtasker",
     CONFIG_CORS:{
         origin:["http://localhost:3000"]
     }

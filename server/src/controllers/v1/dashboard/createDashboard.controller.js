fef3d7774c829f5773ce66a8d3079ef8714e5091
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-07-20 17:59:09 -0500

    refactor:(controller):

--- server/src/controllers/v1/dashboard/createDashboard.controller.js
+++ server/src/controllers/v1/dashboard/createDashboard.controller.js
@@ -1,23 +1,23 @@
 const serviceDashboards = require("../../../database/services/dashboards.service");
 const createDashboard = async (req, res) => {
   try {
-    const name = req.params.name_dashboard;
-    const id_user = req.params.id_user;
-    if (name.trim() === "") {
-      throw new Error("Error: El parametro :name_dashboard esta vacio");
-    }
-    if (isNaN(parseInt(id_user))) {
-      throw new Error("Error: El parametro :id_user esta vacio");
-    }
+    const { id_user } = req;
+    const { name, description } = req.body;
+
     const dashb_new = await serviceDashboards.creatNewDashboard(
       name,
-      "",
+      description,
       id_user
     );
-    console.log(dashb_new);
-    return res.status(200).json(dashb_new);
-  } catch (error) {
-    console.log(error.message);
+
+    return res.status(200).json({
+      succes: true,
+      data: {
+        dashb_new,
+      },
+    });
+  } catch (e) {
+    res.status(500).json({ messageError: e.message });
   }
 };
-module.exports = createDashboard
+module.exports = createDashboard;
--- server/src/controllers/v1/dashboard/getDashboard.controller.js
+++ server/src/controllers/v1/dashboard/getDashboard.controller.js
@@ -1,12 +1,15 @@
 const serviceDashboards = require("../../../database/services/dashboards.service");
 const getDashboards = async (req, res) => {
   try {
-    const id_user = req.params.id_user;
-    const respuesta = await serviceDashboards.getDashboardsByIdUser(id_user);
-    console.log(respuesta);
-    return res.status(200).json(respuesta);
-  } catch (error) {
-    return res.status(500).json(error.message);
+    const { id_user } = req;
+
+    const dashboards_user = await serviceDashboards.getDashboardsByIdUser(
+      id_user
+    );
+    return res.status(200).json({succes:true,data:{dashboards_user}});
+    
+  } catch (e) {
+    return res.status(500).json(e.message);
   }
 };
 

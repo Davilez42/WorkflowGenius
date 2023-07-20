7db2e3fc3f24f9b9d3ecb7ed39b67e345df8f49c
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-07-20 17:54:10 -0500

    feat:add cookie parser

--- server/app.js
+++ server/app.js
@@ -2,15 +2,17 @@ const express = require("express");
 const cors = require("cors");
 
 require("colors"); //* DEV
-
 const routesv1 = require("./src/routes/v1");
 const routeNotFoundHandler = require("./src/middlewares/routeNotFoundHandler");
 const logger = require("./src/middlewares/logger");
+const  cookieParser =  require('cookie-parser')
 const config = require("./src/configs/config");
 const app = express();
 
 app.use(logger);
+// PARSERS
 app.use(express.json());
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
 // CORS
@@ -24,4 +26,5 @@ app.get("/", (req, res) => {
 app.use("/api/v1", routesv1);
 
 app.use(routeNotFoundHandler);
+
 module.exports = app;

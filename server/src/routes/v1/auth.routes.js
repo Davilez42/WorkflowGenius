8b87250e49e269d9d791509695338b8a18bdcc65
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-08-06 14:46:53 -0500

    feat(api): delete dashboard and refactoring

--- server/src/routes/v1/auth.routes.js
+++ server/src/routes/v1/auth.routes.js
@@ -1,10 +1,17 @@
-const Router = require('express')
-const controller = require('../../controllers/v1')
-const validators = require('../../middlewares/paramsValidators.js/')
-const router = Router()
 
-router.post("/sign_user",validators.loginParamsValidator,controller.signUser)
-router.post('/signUp_user',validators.registerParamsValidator,controller.signUpUser)
-module.exports = router
- 
- 
+
+
+
+module.exports = (route, controllers, validators) => {
+
+    route.post("/sign_user", validators.loginParamsValidator, controllers.signUser)
+    route.post('/signUp_user', validators.registerParamsValidator, controllers.signUpUser)
+
+    return route
+}
+
+
+
+
+
+

03b14d7f8eb43599037180179661c3b87b330981
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-09-01 17:48:13 -0500

    fix: not found element

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,12 +1,15 @@
-import { React } from "react";
+import { React, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import NavbarLeft from "./navBarLeft/NavbarLeft";
 import { DashboardContextProvider } from "../context/DashboardContext";
 import "./main.css";
-import loadImage from '../loadRandomImage'
+import loadImage from "../loadRandomImage";
 
 export default function Main() {
-  loadImage()
+  useEffect(() => {
+    loadImage();
+  }, []);
+
   return (
     <DashboardContextProvider>
       <div className="container-main">
--- client/src/loadRandomImage.js
+++ client/src/loadRandomImage.js
@@ -1,5 +1,15 @@
 export default async function loadImage() {
   const container_main = document.querySelector(".container-main");
+  const data = JSON.parse(window.localStorage.getItem('config'))
+
+
+  if (data?.imageBackground) {
+    container_main.style = `background-image: url(${data.imageBackground})`;
+  } else {
+    container_main.style = `background-image: url(https://images.unsplash.com/photo-1637080024867-39547bcbc64a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80)`;
+    window.localStorage.setItem('config', JSON.stringify({ imageBackground: 'https://images.unsplash.com/photo-1637080024867-39547bcbc64a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80' }))
+  }
+
   setInterval(async () => {
     const resp = await fetch(
       "https://api.unsplash.com/photos/random?client_id=yWJ3f_5Njqt88VQus0ybMb7C5sjFrlDONqlMlvkrBhY"
@@ -7,8 +17,11 @@ export default async function loadImage() {
 
     if (resp.ok) {
       const image = await resp.json();
-      if (!container_main) return;
-      container_main.style = `background-image: url(${image.urls.full})`;
+      const url_image = image.urls.full
+      container_main.style = `background-image: url(${url_image})`;
+      window.localStorage.setItem('config', JSON.stringify({ imageBackground: url_image }))
     }
   }, 1000000);
+
+
 }

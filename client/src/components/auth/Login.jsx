616c4cb60525f17eb9fe4a22042bcae1b4353c43
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-09-16 18:49:25 -0500

    feat(component): add button of google and refactor

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -36,35 +36,53 @@ export default function Login() {
 
   return (
     <>
-      <div className="mainForms">
-        <div className="container_form">
-          <form>
-            <h3>Sign in</h3>
+      <div className="container_form">
+        <form>
+          <h3>Sign in</h3>
+          <label htmlFor="input_username"> Usuario o correo</label>
+          <input
+            id="input_username"
+            onChange={(event) => setUsername(event.target.value)}
+            className="input input-username"
+            type="text"
+            placeholder="Username"
+          ></input>
+          <label htmlFor="input_password">Contraseña</label>
+          <input
+            id="input_password"
+            onChange={(event) => setPassword(event.target.value)}
+            className="input input-password"
+            type="password"
+            placeholder="Password"
+          ></input>
+          <NavLink to="/">Olvidastes tu contraseña?</NavLink>
+          <br />
+          <div className="container_message">
+            <p>{message}</p>
+          </div>
+          <div onClick={senDate} className="btn">
+            Login
+          </div>
+        </form>
+        <div className="container_btn_google">
+          <div
+            id="g_id_onload"
+            data-client_id="585334348569-4cln0bonf18g5dpd3jbbnjh1b1nv4vjj.apps.googleusercontent.com"
+            data-context="signin"
+            data-ux_mode="popup"
+            data-login_uri="http://localhost:5000/"
+            data-itp_support="true"
+          ></div>
 
-            <input
-              id="input_username"
-              onChange={(event) => setUsername(event.target.value)}
-              className="input input-username"
-              type="text"
-              placeholder="Username"
-            ></input>
-
-            <input
-              id="input_password"
-              onChange={(event) => setPassword(event.target.value)}
-              className="input input-password"
-              type="password"
-              placeholder="Password"
-            ></input>
-            <NavLink to="/">Forget you password?</NavLink>
-            <br />
-            <div onClick={senDate} className="btn">
-              Login
-            </div>
-            <div className="container_message">
-              <p>{message}</p>
-            </div>
-          </form>
+          <div
+            className="g_id_signin"
+            data-type="standard"
+            data-shape="rectangular"
+            data-theme="outline"
+            data-text="signin_with"
+            data-size="large"
+            data-logo_alignment="left"
+          ></div>
         </div>
         <div className="box_info">
           <p>
--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -15,6 +15,7 @@ export default function Register() {
 
   const clearFields = () => {
     setFirstNames("");
+    setLastNames("");
     setPassword("");
     setEmail("");
     setUsername("");
@@ -35,6 +36,20 @@ export default function Register() {
   const [error, setError] = useState("");
   const handlerSubmit = () => {
     setError("");
+
+    if (first_names.trim() === "") {
+      setError("El nombre es incorrecto");
+      return;
+    }
+    if (last_names.trim() === "") {
+      setError("El apellido es incorrecto");
+      return;
+    }
+    if (email.trim() === "") {
+      setError("El email es incorrecto");
+      return;
+    }
+
     if (username.trim() === "") {
       setError("El username es requerido");
       return;
@@ -43,19 +58,6 @@ export default function Register() {
       setError("La contraseña es demasiado corta");
       return;
     }
-    if (email.trim() === "") {
-      setError("El email es incorrecto");
-      return;
-    }
-    if (first_names.trim() === "") {
-      setError("El first name es incorrecto");
-      return;
-    }
-    if (last_names.trim() === "") {
-      setError("El last names es incorrecto");
-      return;
-    }
-
     setError("");
 
     RegisterHandler(
@@ -73,74 +75,73 @@ export default function Register() {
 
   return (
     <>
-      <div className="mainForms">
-        <div className="container_form">
-          <form>
-            <h3>Create account</h3>
-
-            <input
-              id="input_firstNames"
-              className="input input-firstNames"
-              type="text"
-              placeholder="First names"
-              onChange={(e) => {
-                setFirstNames(e.target.value);
-              }}
-            ></input>
-
-            <input
-              id="input_lastNames"
-              className="input input-lastNames"
-              type="text"
-              placeholder="Last names"
-              onChange={(e) => {
-                setLastNames(e.target.value);
-              }}
-            ></input>
-
-            <input
-              id="input_username"
-              className="input input-username"
-              type="text"
-              placeholder="Username"
-              onChange={(e) => {
-                console.log(username);
-                setUsername(e.target.value);
-              }}
-            ></input>
-
-            <input
-              id="input_email"
-              className="input input-email"
-              type="email"
-              placeholder="Email@some.com"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            ></input>
-
-            <input
-              id="input_password"
-              className="input input-password"
-              type="password"
-              placeholder="Password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            ></input>
-            <div
-              className="btn"
-              onClick={() => {
-                handlerSubmit();
-              }}
-            >
-              Register
-            </div>
-            <div className="container_message">
-              <p>{error}</p>
-            </div>
-          </form>
-        </div>
+      <div className="container_form">
+        <form>
+          <h3>Create account</h3>
+          <label htmlFor="input_firstNames">Nombres *</label>
+          <input
+            id="input_firstNames"
+            className="input input-firstNames"
+            type="text"
+            placeholder="First names"
+            onChange={(e) => {
+              setFirstNames(e.target.value);
+            }}
+          ></input>
+          <label htmlFor="input_lastNames">Apellidos </label>
+          <input
+            id="input_lastNames"
+            className="input input-lastNames"
+            type="text"
+            placeholder="Last names"
+            onChange={(e) => {
+              setLastNames(e.target.value);
+            }}
+          ></input>
+          <label htmlFor="input_username">Nombre de usuario *</label>
+          <input
+            id="input_username"
+            className="input input-username"
+            type="text"
+            placeholder="Username"
+            onChange={(e) => {
+              console.log(username);
+              setUsername(e.target.value);
+            }}
+          ></input>
+          <label htmlFor="input_email">Correo electronico *</label>
+          <input
+            id="input_email"
+            className="input input-email"
+            type="email"
+            required
+            placeholder="Email@some.com"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          ></input>
+          <label htmlFor="input_password">Contraseña *</label>
+          <input
+            id="input_password"
+            className="input input-password"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          ></input>
+          <div className="container_message">
+            <p>{error}</p>
+          </div>
+          <div
+            className="btn"
+            onClick={() => {
+              handlerSubmit();
+            }}
+          >
+            Register
+          </div>
+        </form>
         <div className="box_info">
           <p>
             Already have an account ?{" "}
